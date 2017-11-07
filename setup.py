42d2fce4302caf81fa350010e28cce75b7115be7
maxkoryukov <maxkoryukov@yandex.ru>
2017-11-07 21:11:21 +0500

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 VERSION = (0, 0, 5)
-VERSION_SUFFIX = ''  # 'dev.12'
+VERSION_SUFFIX = 'dev.1'
 
 VERSION_STRING = '.'.join([str(x) for x in VERSION[0:3]])
 RELEASE_STRING = VERSION_STRING + VERSION_SUFFIX
